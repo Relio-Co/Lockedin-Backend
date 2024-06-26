2090b5e21394ac0c9c0d5706defd6942a2aed3e9
Manav M. <kookygavinquack@gmail.com>
2024-06-26 14:42:12 -0400

    restart server

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ const authenticateToken = async (req, res, next) => {
     req.user = decodedToken;
     next();
   } catch (error) {
-    console.error('Error verifying token:', error);
     return res.sendStatus(403);
   }
 };
@@ -42,7 +41,6 @@ const validateAndCreateUser = async (req, res) => {
 
     res.status(200).json(user);
   } catch (error) {
-    console.error('Error validating/creating user:', error);
     res.status(500).json({ error: error.message });
   }
 };
