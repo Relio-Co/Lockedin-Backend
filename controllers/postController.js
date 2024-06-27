56132c22ece232b55d1028103288ded48ada2274
Manav M. <kookygavinquack@gmail.com>
2024-06-27 03:27:57 -0400

    upload

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,26 +1,27 @@
 const { Post } = require('../models');
 
 const createPost = async (req, res) => {
-  try {
-    const { groupId, caption, isPublic } = req.body;
-    const createdBy = req.user.uid;
-    const createdByUsername = req.user.email.split('@')[0];
-    const imageUrl = req.body.imageUrl; // Get imageUrl from request body
-
-    const newPost = await Post.create({
-      group_id: groupId,
-      caption,
-      created_by: createdBy,
-      created_by_username: createdByUsername,
-      image_url: imageUrl,
-      is_public: isPublic
-    });
-
-    res.status(201).json(newPost);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+    try {
+      const { groupId, caption, isPublic } = req.body;
+      const createdBy = req.user.uid;
+      const createdByUsername = req.user.email.split('@')[0];
+      const imageUrl = req.file ? `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}` : null; // Update to use req.file
+  
+      const newPost = await Post.create({
+        group_id: groupId,
+        caption,
+        created_by: createdBy,
+        created_by_username: createdByUsername,
+        image_url: imageUrl,
+        is_public: isPublic
+      });
+  
+      res.status(201).json(newPost);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+  
 
 const getAllPosts = async (req, res) => {
   try {
--- routes/Posts.js
+++ routes/Posts.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const postController = require('../controllers/postController');
+const multer = require('multer');
+const path = require('path');  // Add this line
 const router = express.Router();
 
-router.post('/', postController.createPost);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); // Append the original file extension
+  }
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/', upload.single('image'), postController.createPost);
 router.get('/', postController.getAllPosts);
 
 module.exports = router;
