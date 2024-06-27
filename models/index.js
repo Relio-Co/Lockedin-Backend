c7e21a5b3e13d9c6bc0535a8b68f685cb9a3c441
Manav M. <kookygavinquack@gmail.com>
2024-06-27 02:37:44 -0400

    changed index

--- models/index.js
+++ models/index.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/db');
 const User = require('./userModel');
 const Group = require('./groupModel');
-const GroupMember = require('./groupMemberModel'); // New model for group members
+const GroupMember = require('./groupMemberModel');
 const FriendRequest = require('./friendRequestModel');
+const Friend = require('./friendModel'); // Import the Friend model
 
 // Define associations
 User.hasMany(Group, { foreignKey: 'user_id', sourceKey: 'user_id' });
@@ -19,12 +20,16 @@ User.hasMany(FriendRequest, { foreignKey: 'receiver_id', as: 'ReceivedRequests' 
 FriendRequest.belongsTo(User, { foreignKey: 'sender_id', as: 'Sender' });
 FriendRequest.belongsTo(User, { foreignKey: 'receiver_id', as: 'Receiver' });
 
+// Add the Friend model association
+User.belongsToMany(User, { through: Friend, as: 'Friends', foreignKey: 'user_id', otherKey: 'friend_user_id' });
+
 const db = {
   sequelize,
   User,
   Group,
   GroupMember,
   FriendRequest,
+  Friend, // Add Friend model to the db object
 };
 
 module.exports = db;
