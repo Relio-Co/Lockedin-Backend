77e0363f1adc298d6bf80b45fd47e6d63105c2cc
Manav M. <kookygavinquack@gmail.com>
2024-06-27 03:18:07 -0400

    uploads

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const admin = require('firebase-admin');
+const multer = require('multer');
+const path = require('path');
 require('dotenv').config();
 
 const db = require('./models');
@@ -18,6 +20,17 @@ const PORT = process.env.PORT || 3007;
 
 app.use(express.json());
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); // Append the original file extension
+  }
+});
+
+const upload = multer({ storage: storage });
+
 const authenticateToken = async (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
@@ -34,6 +47,16 @@ const authenticateToken = async (req, res, next) => {
 };
 
 app.post('/user/validate-token', authenticateToken, validateAndCreateUser);
+
+app.post('/upload', authenticateToken, upload.single('image'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+  const fileUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
+  res.status(200).json({ url: fileUrl });
+});
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/user', authenticateToken, userRoute);
 app.use('/groups', authenticateToken, groupsRoute);
