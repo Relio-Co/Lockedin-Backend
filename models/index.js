ecf862da13fa7650350d5544c0f485ff603b9d51
Manav M. <kookygavinquack@gmail.com>
2024-06-27 04:29:20 -0400

    user model

--- models/index.js
+++ models/index.js
@@ -1,34 +1,42 @@
-const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
+const User = require('./userModel');
+const Group = require('./groupModel');
+const GroupMember = require('./groupMemberModel');
+const FriendRequest = require('./friendRequestModel');
+const Friend = require('./friendModel');
+const Post = require('./postModel');
 
-const Group = sequelize.define('Group', {
-  group_id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  name: {
-    type: DataTypes.STRING(255),
-  },
-  public: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: true,
-  },
-  score: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0,
-  },
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'users',
-      key: 'user_id',
-    },
-  },
-}, {
-  tableName: 'groups',
-  timestamps: false,
-});
+// Define associations
+User.hasMany(Group, { foreignKey: 'user_id', sourceKey: 'user_id' });
+Group.belongsTo(User, { foreignKey: 'user_id', targetKey: 'user_id' });
 
-module.exports = Group;
+Group.hasMany(GroupMember, { foreignKey: 'group_id', sourceKey: 'group_id', as: 'GroupMembers' });
+GroupMember.belongsTo(Group, { foreignKey: 'group_id', targetKey: 'group_id' });
+
+User.hasMany(GroupMember, { foreignKey: 'user_id', sourceKey: 'user_id' });
+GroupMember.belongsTo(User, { foreignKey: 'user_id', targetKey: 'user_id' });
+
+User.hasMany(FriendRequest, { foreignKey: 'sender_id', as: 'SentRequests' });
+User.hasMany(FriendRequest, { foreignKey: 'receiver_id', as: 'ReceivedRequests' });
+FriendRequest.belongsTo(User, { foreignKey: 'sender_id', as: 'Sender' });
+FriendRequest.belongsTo(User, { foreignKey: 'receiver_id', as: 'Receiver' });
+
+User.hasMany(Post, { foreignKey: 'created_by', sourceKey: 'uuid' });
+Post.belongsTo(User, { foreignKey: 'created_by', targetKey: 'uuid' });
+
+Group.hasMany(Post, { foreignKey: 'group_id', as: 'Posts' });
+Post.belongsTo(Group, { foreignKey: 'group_id' });
+
+User.belongsToMany(User, { through: Friend, as: 'Friends', foreignKey: 'user_id', otherKey: 'friend_user_id' });
+
+const db = {
+  sequelize,
+  User,
+  Group,
+  GroupMember,
+  FriendRequest,
+  Friend,
+  Post,
+};
+
+module.exports = db;
